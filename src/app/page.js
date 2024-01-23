a3a20b7e8d92f2334556fda791932e3a2f660470
rrauf8178 <rrauf8178@gmail.com>
2024-01-24 03:06:08 +0400

    add common styles and variables in global css file and create navbar footer components import to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-   Hello World
-    </main>
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
   );
 }
