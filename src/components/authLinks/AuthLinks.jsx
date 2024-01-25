b23d76156b2782718e8b04d2a87bf6deb7e75b00
rrauf8178 <rrauf8178@gmail.com>
2024-01-25 20:01:00 +0400

    finish dark mode functiopnality and contex

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import styles from "./authlinks.module.css"
+import React from "react";
+import styles from "./authlinks.module.css";
+import Link from "next/link";
 
 const AuthLinks = () => {
+  const status = "notauthenticated";
   return (
-    <div>AuthLinks</div>
-  )
-}
+    <>
+      {status === "notauthenticated" ? (
+        <Link href={"/login"}>Login</Link>
+      ) : (
+        <>
+          <Link href={"/write"}>Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
+};
 
-export default AuthLinks
+export default AuthLinks;
